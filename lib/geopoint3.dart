93c48df115d30e75433c3f29c124f761190e3b84
Mario Blatarić <mario.blataric@logon.hr>
2023-07-12 11:33:04 +0200

    Updated dependicies, cleaned code warnings

--- lib/geopoint3.dart
+++ lib/geopoint3.dart
@@ -1,7 +1,7 @@
 /// Datastructures and methods for Geolocator. Provides a [GeoPoint]
 /// model for individual points and a [GeoSerie] model for groups, lines
 /// or polygons
-library geopoint2;
+library geopoint3;
 
 export 'src/models/geopoint.dart';
 export 'src/models/geoserie.dart';
